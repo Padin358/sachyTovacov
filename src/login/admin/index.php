db853242d1fc44d42e89f698a425e0271976cce9
Padin358 <padin358@email.cz>
2026-03-09 11:36:28 +0100

    logout popup

--- src/login/admin/index.php
+++ src/login/admin/index.php
@@ -12,16 +12,15 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../../output.css">
     <title>Admin Panel</title>
 </head>
 <body class="bg-[#0a0a0a] text-[#dfdfdf] min-h-screen p-8">
-    <div class="max-w-4xl mx-auto">
+    <div class="">
         <div class="flex justify-between items-center mb-8">
-            <h1 class="text-3xl font-semibold">Šachy Tovačov &raquo; Admin Panel</h1>
-            <a href="logout.php" class="bg-red-600 hover:bg-red-700 px-4 py-2 rounded-lg transition-colors">
-                Odhlásit se
-            </a>
+            <h1 class="text-3xl font-semibold text-blue-400">Šachy Tovačov &raquo; Admin Panel</h1>
+            <button class="bg-blue-400 hover:bg-blue-500 px-4 py-2 rounded-lg transition-colors text-[#dfdfdf] font-semibold js-toggle-popup cursor-pointer">Odhlásit se</button>
         </div>
         
         <div class="bg-[#1d1d1d] p-6 rounded-xl">
@@ -29,5 +28,24 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
             <p>Toto je admin panel. Zde můžete spravovat obsah webu.</p>
         </div>
     </div>
+    <div class="fixed top-0 left-0 h-full w-full bg-black/70 hidden transition-all" id="logoutPopup">
+        <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#1d1d1d] p-3 rounded-lg flex flex-col justify-center gap-5 items-center">
+            <button class="text-red-600 absolute top-1 right-1 cursor-pointer hover:text-red-700 transition-colors js-toggle-popup"><i class="fa-solid fa-x text-xl"></i></button>
+            <p class="text-xl m-3">Opravdu se chcete odhlásit?</p>
+            <a href="logout.php" class="bg-blue-400 hover:bg-blue-500 px-4 py-2 rounded-lg transition-colors text-[#dfdfdf] font-semibold">Odhlásit</a>
+        </div>
+    </div>
+    <script>
+        const toggleButtons = document.querySelectorAll(".js-toggle-popup")
+        const logoutPopup = document.getElementById("logoutPopup")
+
+        const logoutPopupFunction = () => {
+            logoutPopup.classList.toggle("hidden")
+        }
+
+        toggleButtons.forEach(button => {
+            button.addEventListener("click", logoutPopupFunction)
+        })
+    </script>
 </body>
 </html>
